c387d97c2461ad2c7a321ae0ccc9b15f04763f16
hongning <ning.hong@d3j.io>
2020-08-05 11:40:09 +0800

    bugfix: 修复请求太快被限制及优化输出

--- run.py
+++ run.py
@@ -11,25 +11,38 @@ def get_html(url):
     response = requests.get(url)
     return response.text
 
-def analyze_html(html):
+def analyze_html(html, code):
     soup = BeautifulSoup(html, "lxml")
-    tr = soup.find_all("tr")
-    print(tr)
-    tr = tr[2]
-    tds = tr.find_all('td')
-    return tds[1].string,tds[3].string
+    trs = soup.find_all("tr")
+    if len(trs) < 3:
+        return None, None
+    for tr in trs:
+        td = tr.find_all('td')
+        if td[1].string == code:
+            #print(td[1].string, td[3].string)
+            return td[1].string, td[3].string
+    return None,None
 
 
 def store_text(code, name):
     with open('result.txt', 'a')  as f:
         f.write("%s----%s\n"%(code,name))
 
+def store_err_text(code):
+    with open('error.txt', 'a') as f:
+        f.write("%s 三字码对应机场名未找到\n"%(code))
+
 for v in stationMap.values():
-    print("正在进行 %s 三字码对应机场查询...")
+    print("正在进行 %s 三字码对应机场查询..."%(v))
     spec_url = url + v + suffix
     #print(spec_url)
     html = get_html(spec_url)
-    code, name = analyze_html(html)
+    code, name = analyze_html(html, v)
     #print(code, name)
-    store_text(code, name) 
+    if code and name:
+        print("三字码: %s 对应机场为: %s"%(code, name))
+        store_text(code, name) 
+    else:
+        print("三字码: %s 对应信息没找到"%(v))
+        store_err_text(v)
     time.sleep(2)
